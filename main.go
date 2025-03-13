3ce173fb39eebb35e22380474cac07e0d48bad50
nxhettry <nishan.gtsn@gmail.com>
2025-03-13 19:41:57 +0545

    created a basic ge t enpoint

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 
 func main() {
 	fmt.Print("Hello World")
-
 	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello this is a reponse from the route"})
+	})
 
 	log.Fatal(app.Listen(":8080"))
 }
