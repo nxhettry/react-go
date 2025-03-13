2e7c2ed6d989dbb6b31ec76d425da194c5046dea
nxhettry <nishan.gtsn@gmail.com>
2025-03-13 14:25:19 +0545

    creating the golan=g setup by learning

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nxhettry/react-go
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World")
+
+	var myName string = "Nishan"
+
+	const myCaste string = "Gautam"
+
+	myThirdNamme := "Nishan Vai"
+
+	fmt.Print(myName, myCaste, myThirdNamme)
+
+}
